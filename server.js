c4b3271b8ea57b0c6a5f7483dc6a30e39db3c455
Isha1814 <135538382+Isha1814@users.noreply.github.com>
2024-12-05 10:21:18 +0530

    changes

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
 import { config } from 'dotenv';
+import router from './router/router.js';
 
 const app = express()
 
@@ -13,7 +14,11 @@ config();
 
 /** application port */
 const port = process.env.PORT || 8080;
+
+
 /** routes */
+app.use('/api', router) /**api's */
+
 app.get('/', (req, res) => {
     try{
         res.json("Get Request")
